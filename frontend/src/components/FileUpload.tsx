041d5c1cb1b9c5f0fbff465d6476aa8c84f66ee7
Saketh Pabolu <51687055+spabolu@users.noreply.github.com>
2024-11-17 16:24:55 -0700

    cleanup backend put in folder

--- frontend/src/components/FileUpload.tsx
+++ frontend/src/components/FileUpload.tsx
@@ -77,7 +77,7 @@ export default function FileUpload(): JSX.Element {
 
             console.log(`Uploading ${file.name} to the backend...`);
             // Send POST request to upload .zip file
-            const response = await fetch('http://127.0.0.1:5000/upload', {
+            const response = await fetch('http://52.38.228.48:5000/upload', {
               method: "POST",
               body: formData,
             });
