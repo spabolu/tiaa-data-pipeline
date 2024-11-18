4d9a864367f40f6103f99896728f3440f046b33d
Ubuntu <ubuntu@ip-172-26-8-114.us-west-2.compute.internal>
2024-11-18 06:46:13 +0000

    final

--- frontend/src/components/Insights.tsx
+++ frontend/src/components/Insights.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
 
 export default function Insights() {
   const [pdfUrl, setPdfUrl] = useState<string | null>(null);
@@ -7,7 +8,7 @@ export default function Insights() {
   useEffect(() => {
     const fetchPdf = async () => {
       try {
-        const response = await fetch('http://52.38.228.48:5000/download_report', {
+        const response = await fetch('http://52.38.228.48:5000/download_pdf', {
           method: 'GET',
         });
 
@@ -43,6 +44,52 @@ export default function Insights() {
           ) : (
             <p>Loading PDF...</p>
           )}
+  
+            <Button
+              onClick={async () => {
+                const response = await fetch('http://52.38.228.48:5000/download_transcript');
+                if (response.ok) {
+                  const blob = await response.blob();
+                  const url = URL.createObjectURL(blob);
+                  window.open(url, '_blank'); // Open the HTML file in a new tab
+                } else {
+                  console.error('Failed to fetch transcript.html');
+                }
+              }}
+              className="text-center text-zinc-800 mt-4"
+            >
+              Open transcript.html
+            </Button>
+            <Button
+              onClick={async () => {
+                const response = await fetch('http://52.38.228.48:5000/download_profile');
+                if (response.ok) {
+                  const blob = await response.blob();
+                  const url = URL.createObjectURL(blob);
+                  window.open(url, '_blank'); // Open the HTML file in a new tab
+                } else {
+                  console.error('Failed to fetch profile.html');
+                }
+              }}
+              className="text-center text-zinc-800 mt-4"
+            >
+              Open profile.html
+            </Button>
+            <Button
+              onClick={async () => {
+                const response = await fetch('http://52.38.228.48:5000/download_portfolio');
+                if (response.ok) {
+                  const blob = await response.blob();
+                  const url = URL.createObjectURL(blob);
+                  window.open(url, '_blank'); // Open the HTML file in a new tab
+                } else {
+                  console.error('Failed to fetch portfolio.html');
+                }
+              }}
+              className="text-center text-zinc-800 mt-4"
+            >
+              Open portfolio.html
+            </Button>
         </CardContent>
       </Card>
     </div>
